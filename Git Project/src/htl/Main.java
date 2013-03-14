a3c91f11e7a3cf662f12be4341a52e2130f3aed3
Stephan <stone.ey@gmx.at>
2013-03-14 11:49:11 +0100

    First Version Create

---
+++ Git Project/src/htl/Main.java
@@ -0,0 +1,13 @@
+package htl;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("---------- Project Name ----------");
+
+	}
+
+}
